dabc99747d409c5b9b8212d0cbd2ccb0a0fbf87d
Flodagnas <74758873+Flodagnas@users.noreply.github.com>
2022-02-07 22:04:38 +0100

    Update test.js

--- scripts/js/test.js
+++ scripts/js/test.js
@@ -1,16 +1,38 @@
-import axios from "axios";
+console.log("Script display loaded")
+	
+	let linesInTab = []
+	let pageNumero = 0
+	let select = document.getElementById('nb-elem').value
+	
+	callJson()
+	
+	// Traitement du tableu de données
+	function loadTab(persoes) {
+	    deleteLines()
+	
+	    for (let i in persoes) {
+	        let perso = persoes[i]
+	        if (search != "") {
+	            if (eval('perso.' + searchSelect + '.includes(search)')) {
+	                makeLine(perso)
+	            }
+	        } else {
+	            makeLine(perso)
+	        }
+	    }
+	}
+	
+	// Chargement du JSON et des fonctions associées
+	function callJson() {
+	    fetch('https://swapi.dev/api/people')
+	    .then((response) => response.json()) // parse the response from JSON
+	    .then(loadTab) // .then will call the function with the JSON value
+	    .then(() => {
+	        linesInTab = []
+	        document.querySelectorAll('.line').forEach(tr => {
+	            linesInTab.push(tr)
+	        })
+	    })
+	    .then(pagination)
+	}
 
-const TestAPI = axios.create({
-    baseURL: "https://swapi.dev/api/",
-    headers: {
-        "Content-Type": "application/json",
-    },
-});
-
-export default class API{
-
-    static async APIPeople(){
-        
-    }
-
-}
