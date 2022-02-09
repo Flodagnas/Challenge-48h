c858a56da33024cec1b546cce05ab1e2d3e47937
root <root@LAPTOP-8BN54FDS.localdomain>
2022-02-09 05:41:14 +0100

    final test

--- scripts/js/films.js
+++ scripts/js/films.js
@@ -24,30 +24,29 @@ const loadTab = (persoes) => {
     table.appendChild(tbody);
 };
 
-function buffedFetch(obj, property){
+async function buffedFetch(obj, property){
     var link1 = new Array()
     var link2 = new Array()
     var result = new Array()
     var result2 = new Array
-    console.log(obj[5][property][0]);
+    //console.log(obj[5][property][0]);
     for (let j=0; j<6; j++){ 
         link1 = obj[j][property];
         for (let i = 0; i <link1.length; i++ ){
         //console.log("j=", j,"i=", i, link1[i]);
         link2 = link1[i]
         //console.log("link2", link2);
-        fetch(link2).then(response =>{
-           return response.json()
-        }).then(response =>{
+        let response = await fetch(link2);
+        let response1 = await response.json();
           //console.log("f", response.name, "t", typeof response.name);
-          result[i] = response.name 
-          //console.log("j=",j, 'i=',i, response.name);
-        })
+        result[i] = response1.name 
+        //console.log("j=",j, 'i=',i, response1.name);
         //console.log(result2)
         result2[j] = result
         }   
+        result = []
       }      
-      console.log(result2);
+     return result2;
 };
 
 // Chargement du JSON et des fonctions associées
@@ -58,7 +57,12 @@ const callJson = async (page = pageNumero) => {
       loadTab(films.results);
       page += 1
       /*console.log((films.results));*/
-      buffedFetch(films.results, 'species');
+    species = await buffedFetch(films.results, "species")
+     console.log(species);
+      /*console.log(buffedFetch(films.results, 'planets').then);
+      console.log(buffedFetch(films.results, 'vehicles').then);
+      console.log(buffedFetch(films.results, 'starships').then;
+      console.log(buffedFetch(films.results, 'people'));*/
       /*=for (var j=0; j<6; j++){ 
       link1 = films.results[j].species;
       for (var i = 0; i < link1.length; i++ ){
